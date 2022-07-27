ecd489910efc5907feac0b7cea8be96e752c3042
Octavio Lara <octavioasapchi@gmail.com>
2022-07-27 03:25:05 -0400

    [:wrench: fix] user login
    [:wrench: update] force lowercase to member password

--- controllers/members.js
+++ controllers/members.js
@@ -5,50 +5,50 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const createMember = async (req, res) => {
-	try {
-		const { firstName, lastname, membership, email } = req.body;
-		if (!firstName || !lastname || !membership || !email) {
-			return res.status(400).send({
-				error: "Email, first name, lastname and membership are required",
-			});
-		}
-		const clubId = req.user.clubId;
-		const clubMembers = await Member.find({ clubId });
-		const memberInUse = clubMembers.some(
-			(elm) => elm.membership === membership
-		);
+  try {
+    const { firstName, lastname, membership, email } = req.body;
+    if (!firstName || !lastname || !membership || !email) {
+      return res.status(400).send({
+        error: "Email, first name, lastname and membership are required",
+      });
+    }
+    const clubId = req.user.clubId;
+    const clubMembers = await Member.find({ clubId });
+    const memberInUse = clubMembers.some(
+      (elm) => elm.membership === membership
+    );
 
-		const isUser = await User.findOne({ email });
-		if (memberInUse) {
-			return res
-				.status(406)
-				.send({ error: "The membership number is already taken" });
-		}
-		if (isUser) {
-			return res.status(400).send({ error: "This email already in use" });
-		}
-		const memberResponse = await Member.create({
-			firstName,
-			lastname,
-			membership,
-			email,
-			clubId,
-		});
+    const isUser = await User.findOne({ email });
+    if (memberInUse) {
+      return res
+        .status(406)
+        .send({ error: "The membership number is already taken" });
+    }
+    if (isUser) {
+      return res.status(400).send({ error: "This email already in use" });
+    }
+    const memberResponse = await Member.create({
+      firstName,
+      lastname,
+      membership,
+      email,
+      clubId,
+    });
 
-		const provitionalPassword = `${firstName}${membership}`;
-		const hashedPassword = await bcrypt.hash(provitionalPassword, saltRounds);
-		const user = await User.create({
-			email,
-			password: hashedPassword,
-			role: "member",
-			clubId: clubId,
-			memberId: memberResponse._id,
-		});
+    const provitionalPassword = `${firstName.toLowerCase()}${membership}`;
+    const hashedPassword = await bcrypt.hash(provitionalPassword, saltRounds);
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      role: "member",
+      clubId: clubId,
+      memberId: memberResponse._id,
+    });
 
-		return res.status(200).send({ user, member: memberResponse });
-	} catch (err) {
-		console.log(err);
-	}
+    return res.status(200).send({ user, member: memberResponse });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { createMember };
--- controllers/users.js
+++ controllers/users.js
@@ -6,33 +6,37 @@ const Club = require("../models/Club");
 const { generateAccessToken } = require("../middlewares/auth");
 
 const login = async (req, res) => {
-	const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-	if (!email || !password) {
-		return res.status(406).send({ error: "All fields are required" });
-	}
+    if (!email || !password) {
+      return res.status(406).send({ error: "All fields are required" });
+    }
 
-	const user = await User.findOne({ email });
-	if (!user) {
-		return res.status(404).send({ error: "User not found" });
-	}
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
 
-	let token;
-	if (bcrypt.compare(password, user.password)) {
-		token = generateAccessToken({
-			role: user.role,
-			email: user.email,
-			memberId: user.memberId || null,
-			clubId: user.clubId,
-		});
-	} else {
-		return res.status(406).send({ error: "Incorrect Password" });
-	}
-	return res.json({ token });
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      const token = generateAccessToken({
+        role: user.role,
+        email: user.email,
+        memberId: user.memberId || null,
+        clubId: user.clubId,
+      });
+      return res.json({ token });
+    } else {
+      return res.status(406).send({ error: "Incorrect Password" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getUserData = (req, res) => {
-	return res.status(200).send({ msg: "Sos alto pro" });
+  return res.status(200).send({ msg: "Sos alto pro" });
 };
 
 module.exports = { login, getUserData };
