59e021c75b5d6f805f8b4ce8d6597c22e0d27612
BrnMarq <BrnMarq@gmail.com>
2022-07-28 04:23:32 -0400

    [✨ Add] getClub and updateClub endpoints

--- routes/clubs.js
+++ routes/clubs.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createClub, getMembers } = require("../controllers/clubs");
-const { tokenValidate } = require("../middlewares/auth");
+const {
+	createClub,
+	getMembers,
+	updateClub,
+	getClub,
+} = require("../controllers/clubs");
+const { tokenValidate, requireAdmin } = require("../middlewares/auth");
 
 router.post("/create", createClub);
+router.get("/", tokenValidate, requireAdmin, getClub);
 router.get("/members", tokenValidate, getMembers);
+router.put("/:id", tokenValidate, updateClub);
 
 module.exports = router;
