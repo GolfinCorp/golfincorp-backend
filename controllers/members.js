6054da7d3aefa8b87ff0636156271ada3952e186
BrnMarq <BrnMarq@gmail.com>
2022-07-28 04:28:48 -0400

    [♻️ Refactor] updateMember and createMember endpoints logic

--- controllers/members.js
+++ controllers/members.js
@@ -5,108 +5,109 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const createMember = async (req, res) => {
-  try {
-    if (req.user.role !== "admin") {
-      return res.status(401).send({ error: "Administrator rights required" });
-    }
+	try {
+		const { body, user } = req;
+		const { firstName, lastname, membership, email } = body;
+		if (!firstName || !lastname || !membership || !email) {
+			return res.status(400).send({
+				error: "Email, first name, lastname and membership are required",
+			});
+		}
 
-    const { firstName, lastname, membership, email } = req.body;
-    if (!firstName || !lastname || !membership || !email) {
-      return res.status(400).send({
-        error: "Email, first name, lastname and membership are required",
-      });
-    }
+		const clubId = user.clubId;
+		const isMembershipTaken = await Member.findOne({ clubId, membership });
+		const isUserTaken = await User.findOne({ email });
 
-    const clubId = req.user.clubId;
-    const clubMembers = await Member.find({ clubId });
-    const memberInUse = clubMembers.some(
-      (elm) => elm.membership === membership
-    );
+		if (isMembershipTaken) {
+			return res
+				.status(406)
+				.send({ error: "The membership number is already taken" });
+		}
+		if (isUserTaken) {
+			return res.status(400).send({ error: "This email already in use" });
+		}
+		const memberResponse = await Member.create({
+			firstName,
+			lastname,
+			membership,
+			email,
+			clubId,
+		});
 
-    const isUser = await User.findOne({ email });
-    if (memberInUse) {
-      return res
-        .status(406)
-        .send({ error: "The membership number is already taken" });
-    }
-    if (isUser) {
-      return res.status(400).send({ error: "This email already in use" });
-    }
-    const memberResponse = await Member.create({
-      firstName,
-      lastname,
-      membership,
-      email,
-      clubId,
-    });
+		const provitionalPassword = `${firstName.toLowerCase()}${membership}`;
+		const hashedPassword = await bcrypt.hash(provitionalPassword, saltRounds);
+		const newUser = await User.create({
+			email,
+			password: hashedPassword,
+			role: "member",
+			clubId: clubId,
+			memberId: memberResponse._id,
+		});
 
-    const provitionalPassword = `${firstName.toLowerCase()}${membership}`;
-    const hashedPassword = await bcrypt.hash(provitionalPassword, saltRounds);
-    const user = await User.create({
-      email,
-      password: hashedPassword,
-      role: "member",
-      clubId: clubId,
-      memberId: memberResponse._id,
-    });
-
-    return res.status(200).send({ user, member: memberResponse });
-  } catch (err) {
-    return res.status(500).send({ error: err });
-  }
+		return res.status(200).send({ user: newUser, member: memberResponse });
+	} catch (err) {
+		return res.status(500).send({ error: err });
+	}
 };
 
 const updateMember = async (req, res) => {
-  try {
-    const { firstName, lastname, membership } = req.body;
-    const member = await Member.findOne({ _id: req.params.memberId });
+	try {
+		const { body, params, user } = req;
+		const { firstName, lastname, membership } = body;
+		const member = await Member.findOne({
+			_id: params.memberId,
+			clubId: user.clubId,
+		});
 
-    // Fields validation
-    if (!req.params.memberId) {
-      return res.status(400).send({ msg: "Member Id is required" });
-    } else if (!firstName && !lastname && !membership) {
-      return res.status(400).send({
-        msg: "Either the firstname, lastname or membership are required to update",
-      });
-    } else if (!member) {
-      return res.status(404).send({
-        error: "Member id does not match any existing member",
-      });
-    }
-    const updatedMember = await Member.findOneAndUpdate(req.body);
+		// Fields validation
+		if (!firstName && !lastname && !membership) {
+			return res.status(400).send({
+				msg: "Either the firstname, lastname or membership are required to update",
+			});
+		}
+		if (!member) {
+			return res.status(404).send({
+				error: "Member id does not match any existing member",
+			});
+		}
 
-    return res
-      .status(200)
-      .send({ msg: "Member updated successfully", updatedMember });
-  } catch (err) {
-    return res.status(500).send({ error: err });
-  }
+		const updatedMember = await Member.findByIdAndUpdate(member._id, body, {
+			new: true,
+		});
+
+		return res
+			.status(200)
+			.send({ msg: "Member updated successfully", member: updatedMember });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send({ error: err });
+	}
 };
 
 const deleteMember = async (req, res) => {
-  try {
-    const { memberId } = req.body;
-    if (req.user.role !== "admin") {
-      return res.status(401).send({ error: "Administrator rights required" });
-    } else if (!memberId) {
-      return res
-        .status(400)
-        .send({ error: "Membership identifier is required" });
-    }
+	try {
+		const { memberId } = req.body;
+		if (req.user.role !== "admin") {
+			return res.status(401).send({ error: "Administrator rights required" });
+		} else if (!memberId) {
+			return res
+				.status(400)
+				.send({ error: "Membership identifier is required" });
+		}
 
-    const member = await Member.findOneAndDelete({ _id: memberId });
-    if (!member) {
-      return res
-        .status(404)
-        .send({ error: "Member id does not match any existing ID" });
-    } else {
-      return res
-        .status(200)
-        .send({ msg: "Member deleted successfully", response: member });
-    }
-  } catch (err) {
-    return res.status(500).send({ error: err });
-  }
+		const member = await Member.findOneAndDelete({ _id: memberId });
+		if (!member) {
+			return res
+				.status(404)
+				.send({ error: "Member id does not match any existing ID" });
+		} else {
+			return res
+				.status(200)
+				.send({ msg: "Member deleted successfully", response: member });
+		}
+	} catch (err) {
+		return res.status(500).send({ error: err });
+	}
 };
 
 module.exports = { createMember, updateMember, deleteMember };
--- routes/members.js
+++ routes/members.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createMember,
-  deleteMember,
-  updateMember,
+	createMember,
+	deleteMember,
+	updateMember,
 } = require("../controllers/members");
-const { tokenValidate } = require("../middlewares/auth");
+const { tokenValidate, requireAdmin } = require("../middlewares/auth");
 
-router.use(tokenValidate);
+router.use(tokenValidate, requireAdmin);
 router.post("/create", createMember);
 router.delete("/delete", deleteMember);
 router.put("/:memberId", updateMember);
