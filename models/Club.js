ff502553c0401b8b073ed24240c0cd6de22c9ed5
Octavio Lara <octavioasapchi@gmail.com>
2022-07-29 11:36:33 -0400

    [:card_file_box: update] club model for payments

--- models/Club.js
+++ models/Club.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
 const ClubSchema = new mongoose.Schema({
-	email: { type: String, required: true },
-	name: { type: String, required: true, maxLenght: 50 },
-	state: { type: String, required: true, maxLength: 60 },
-	country: { type: String, required: true, maxLength: 50 },
-	subscription: {
-		startDate: { type: Date },
-		billingDate: { type: Date },
-		bills: [Object],
-		plan: { type: String },
-		price: { type: Number },
-	},
+  email: { type: String, required: true },
+  name: { type: String, required: true, maxLenght: 50 },
+  state: { type: String, required: true, maxLength: 60 },
+  country: { type: String, required: true, maxLength: 50 },
+
+  // New club proposition
+  maxParty: { type: Number, required: true },
+  guestPrice: { type: Number, required: true },
+
+  subscription: {
+    startDate: { type: Date },
+    billingDate: { type: Date },
+    bills: [Object],
+    plan: { type: String },
+    price: { type: Number },
+  },
 });
 
 const Club = mongoose.model("Club", ClubSchema);
