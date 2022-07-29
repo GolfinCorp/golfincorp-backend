991a9dacecb3432782a3e8f27778482107633c59
Octavio Lara <octavioasapchi@gmail.com>
2022-07-29 11:37:32 -0400

    [:sparkles: add] canStart custom method

--- models/Game.js
+++ models/Game.js
@@ -1,20 +1,33 @@
 const mongoose = require("mongoose");
 
-const GameSchema = new mongoose.Schema({
-  memberId: { type: String, required: true },
-  clubId: { type: String, required: true },
-  membership: { type: String, required: true },
-  memberName: { type: String, required: true },
-  date: { type: String, required: true },
-  status: { type: String, default: "oncoming" },
-  guests: [
-    {
-      name: { type: String, required: true },
-      membership: { type: String },
-      payment: { type: Number },
+const GameSchema = new mongoose.Schema(
+  {
+    memberId: { type: String, required: true },
+    clubId: { type: String, required: true },
+    membership: { type: String, required: true },
+    memberName: { type: String, required: true },
+    date: { type: String, required: true },
+    status: { type: String, default: "oncoming" },
+    guests: [
+      {
+        name: { type: String, required: true },
+        membership: { type: String },
+        bill: { type: Number },
+        paymentId: { type: Number },
+      },
+    ],
+  },
+  {
+    methods: {
+      canStart() {
+        let unpayedGuest = this.guests.filter(
+          (guest) => !guest.paymentId && !guest.membership
+        );
+        return unpayedGuest;
+      },
     },
-  ],
-});
+  }
+);
 
 const Game = mongoose.model("Game", GameSchema);
 
