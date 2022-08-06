8f24d7e075e87a4f173418a46241646929cdddd7
BrnMarq <BrnMarq@gmail.com>
2022-08-06 01:39:48 -0400

    [♻️ Refactor] Games controllers and routes

--- controllers/games.js
+++ controllers/games.js
@@ -1,110 +1,126 @@
 const Game = require("../models/Game");
 const Member = require("../models/Members");
-const router = require("../routes/games");
-const ObjectId = require("mongoose").Types.ObjectId;
 
 const createGame = async (req, res) => {
-  try {
-    const { date, guests } = req.body;
-    if (!date) {
-      return res.status(400).send({ error: "la fecha es necesaria" });
-    }
-    const formatDate = new Date(date);
-    const member = await Member.findOne({ _id: req.user.memberId });
+	try {
+		const { body, user } = req;
+		const { date, guests } = body;
+		if (user.role === "admin") {
+			return res
+				.status(406)
+				.send({ error: "Can't create a game with an admin account" });
+		}
+		if (!date) {
+			return res.status(400).send({ error: "The date field is needed" });
+		}
+		const formatDate = new Date(date);
+		const member = await Member.findOne({ _id: user.memberId });
 
-    const gameResponse = await Game.create({
-      clubId: req.user.clubId,
-      memberId: member._id,
-      membership: member.membership,
-      memberName: member.firstName,
-      date: formatDate,
-      guests,
-    });
+		const game = await Game.create({
+			clubId: user.clubId,
+			memberId: member._id,
+			membership: member.membership,
+			memberName: member.firstName,
+			date: formatDate,
+			guests,
+		});
 
-    return res.status(200).send({ game: gameResponse });
-  } catch (err) {
-    console.log(err);
-  }
+		return res.status(200).send(game);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: error.message });
+	}
 };
 
 // ? If member gets all his game, else get all the club games
 const getGames = async (req, res) => {
-  try {
-    const games = await Game.find(
-      req.user.memberId
-        ? { memberId: req.user.memberId }
-        : { clubId: req.user.clubId }
-    );
-    return res.status(200).send(games);
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		const { user } = req;
+		const isMember = Boolean(user.memberId);
+		const games = await Game.find(
+			isMember ? { memberId: user.memberId } : { clubId: user.clubId }
+		);
+		return res.status(200).send(games);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: error.message });
+	}
 };
 
 const manageGame = async (req, res) => {
-  try {
-    const { params, body, user } = req;
-    let { status } = body;
+	try {
+		const { params, body, user } = req;
+		let { status } = body;
 
-    if (user.role === "member") {
-      status = "cancelled";
-    }
+		if (user.role === "member") {
+			status = "cancelled";
+		}
 
-    if (!status) {
-      return res.status(400).send({ error: "Game status and id are required" });
-    }
+		if (!status) {
+			return res.status(400).send({ error: "Game status and id are required" });
+		}
 
-    const gameExists = await Game.findOne({ _id: params.id });
-    if (!gameExists) {
-      return res
-        .status(404)
-        .send({ error: "Game id does not match any existing game" });
-    }
+		const gameExists = await Game.findOne({ _id: params.id });
+		if (!gameExists) {
+			return res
+				.status(404)
+				.send({ error: "Game id does not match any existing game" });
+		}
 
-    const gameRespone = await Game.findByIdAndUpdate(
-      params.id,
-      { status },
-      { new: true }
-    );
-    return res.status(200).send(gameRespone);
-  } catch (err) {
-    console.log(err);
-  }
+		const game = await Game.findByIdAndUpdate(
+			params.id,
+			{ status },
+			{ new: true }
+		);
+		return res.status(200).send(game);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: error.message });
+	}
 };
 
 const addGuests = async (req, res) => {
-  const { body, params, user } = req;
-  const gameExists = await Game.findOne({ _id: `${params.id}` });
-  if (!gameExists) {
-    return res
-      .status(404)
-      .send({ error: "Guest id doesn't match any existing game" });
-  }
-  if (!body.guests) {
-    return res.status(400).send({ error: "At least 1 guest is required" });
-  }
-  if (!Array.isArray(body.guests) || !body.guests.length) {
-    return res.status(400).send({ error: "Guests must not be an empty array" });
-  }
-  return res.status(200).send({ msg: "Success" });
+	try {
+		const { body, params } = req;
+		const gameExists = await Game.findOne({ _id: params.id });
+		if (!gameExists) {
+			return res
+				.status(404)
+				.send({ error: "Game id doesn't match any existing game" });
+		}
+		if (!body.guests) {
+			return res.status(400).send({ error: "At least 1 guest is required" });
+		}
+		if (!Array.isArray(body.guests) || !body.guests.length) {
+			return res
+				.status(400)
+				.send({ error: "Guests must not be an empty array" });
+		}
+		return res.status(200).send({ msg: "Success" });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: error.message });
+	}
 };
 
 const deleteGame = async (req, res) => {
-  try {
-    const gameExist = await Game.findOne({ _id: req.params.id });
-    if (!gameExist) {
-      return res
-        .status(404)
-        .send({ error: "Game id provided does not match any existing game" });
-    }
+	try {
+		const { params } = req;
+		const gameExist = await Game.findOne({ _id: params.id });
+		if (!gameExist) {
+			return res
+				.status(404)
+				.send({ error: "Game id provided does not match any existing game" });
+		}
 
-    const deleteResponse = await Game.deleteOne({ _id: req.params.id });
-    return res
-      .status(200)
-      .send({ msg: "Game deleted succesfully", deleteResponse });
-  } catch (error) {
-    console.log(error);
-  }
+		const deleteResponse = await Game.deleteOne({ _id: params.id });
+		return res
+			.status(200)
+			.send({ msg: "Game deleted succesfully", deleteResponse });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ error: error.message });
+	}
 };
 
 module.exports = { createGame, getGames, manageGame, deleteGame, addGuests };
--- routes/games.js
+++ routes/games.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const { tokenValidate } = require("../middlewares/auth");
 const { idValidator } = require("../middlewares/validations");
 const {
-  createGame,
-  getGames,
-  manageGame,
-  deleteGame,
-  addGuests,
+	createGame,
+	getGames,
+	manageGame,
+	deleteGame,
+	addGuests,
 } = require("../controllers/games");
 
 router.use(tokenValidate);
 router.get("/", getGames);
+router.post("/", createGame);
 router.delete("/:id", idValidator, deleteGame);
-router.post("/create", createGame);
-router.put("/manage/:id", manageGame);
-router.patch("/manage/add/:id", idValidator, addGuests);
+router.post("/:id", idValidator, manageGame);
+router.post("/:id/add-guest", idValidator, addGuests);
 
 module.exports = router;
